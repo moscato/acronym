92b83e812c0f91b8959d0b6557fa75626b07d747
jMoscato <110347584+moscato@users.noreply.github.com>
2022-07-31 12:40:12 -0600

    Create bkgr.js

---
+++ bkgr.js
@@ -0,0 +1,25 @@
+// https://denvergov.org/files/assets/public/global-ccd/images/bg-images/16th-st-piano.jpg?w=480
+const rand = Math.floor(Math.random() * 5);
+let html = document.querySelector('html');
+
+function background() {
+    switch (rand) {
+        case (0) : html.style.backgroundImage = "url('images/union-station.jpg')";
+        break;
+        case (1) : html.style.backgroundImage = "url('images/denver-blue-dusk-bg.jpg')";
+        break;
+        case (2) : html.style.backgroundImage = "url('images/denverskyline.jpg')";
+        break;
+        case (3) : html.style.backgroundImage = "url('images/spring-downtown-denver.jpg')";
+        break;
+        case (4) : html.style.backgroundImage = "url('images/fireworks-downtown-denver.jpg')";
+        break;
+
+
+
+        default : html.style.backgroundImage = "url('images/denverskyline.jpg')";
+    }
+}
+
+
+background();
